1b630a5c0ad5c1affc87400f16819e9aa8bfcef0
Seung Bin . Ko <kosb15@naver.com>
2020-08-10 16:28:36 +0900

    기본 디렉토리 구조 구성

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    BrowserRouter,
+    Route,
+    Switch
+} from 'react-router-dom';
+import { ChatConstructor, ChatRoomDashboard } from './components/pages';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Route exact path="/" component={ChatRoomDashboard} />
+            <Switch>
+                <Route path="/rooms" component={ChatConstructor} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
