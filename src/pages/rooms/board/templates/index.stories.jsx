4c7ad25d967d5b29e6abd761db446f3612d1deab
Seung Bin . Ko <kosb15@naver.com>
2020-08-12 16:21:20 +0900

    Storybook 설정 추가

---
+++ src/pages/rooms/board/templates/index.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import RoomBoardTemplate from './index';
+
+export default {
+    title: 'pages/rooms/board',
+    component: RoomBoardTemplate
+};
+
+export const standard = (args) => <RoomBoardTemplate {...args} />;
