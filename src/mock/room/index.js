1e522fecc4dd069a67798477aa2fbdf9dd5abcb2
Seung Bin . Ko <kosb15@naver.com>
2020-08-12 18:41:08 +0900

    feat: /rooms 요청에 대한 mock 예시 추가

--- src/mock/room/index.js
+++ src/mock/room/index.js
@@ -1,9 +1,32 @@
-function mockGetRooms(mock) {
-    mock.onGet('/rooms').reply(200, {
-        users: [[{ id: 1, text: 'hello world' }, { id: 2, text: 'hi' }]]
-    });
+function getRooms(mock) {
+    mock.onGet('/rooms')
+        .reply(200, {
+            rooms: [{ id: 1, text: 'hello world' }, { id: 2, text: 'hi' }]
+        });
+}
+
+function createRooms(mock) {
+    mock.onPost('/rooms')
+        .reply(201, {
+            id: 23, text: 'good'
+        });
 }
 
 export default {
-    mockGetRooms
+    getRooms, createRooms
 };
+
+/* or
+function init(mock) {
+    mock.onGet('/rooms')
+        .reply(200, {
+            rooms: [{ id: 1, text: 'hello world' }, { id: 2, text: 'hi' }]
+        })
+        .onPost('/rooms')
+        .reply(201, {
+            id: 23, text: 'good'
+        });
+}
+
+export default { init };
+*/
