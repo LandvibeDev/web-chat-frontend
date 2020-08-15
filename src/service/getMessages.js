ab120a26f5cf2d86e54939c83da25bd25910d476
Seung Bin . Ko <kosb15@naver.com>
2020-08-15 17:13:24 +0900

    feat: Message 컴포넌트에 시간 정보만 노출하도록 createdAt 전처리 (#20)

--- src/service/getMessages.js
+++ src/service/getMessages.js
@@ -1,9 +1,22 @@
 import axios from 'axios';
 
+function toTime(createdAt) {
+    const date = new Date(createdAt);
+    return date.toLocaleTimeString();
+}
+
+function transformDate(message) {
+    return {
+        ...message,
+        createdAt: toTime(message.createdAt)
+    };
+}
+
 async function getMessages(id) {
     const response = await axios.get(`/api/rooms/${id}`);
     if (response.status === 200) {
-        return response.data;
+        const { messages } = response.data;
+        return messages.map(transformDate);
     }
 
     throw Error('존재하지 않는 채팅방입니다.');
