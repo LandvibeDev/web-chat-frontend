08d2e2e155dc4c4e56fc364442f43fab154111d3
Seung Bin . Ko <kosb15@naver.com>
2020-08-27 16:55:10 +0900

    feat: Message 입력받기 위한 Input 컴포넌트 추가 (#25)

---
+++ src/components/atoms/Input.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import PropTypes from 'prop-types';
+
+const StyledInput = styled.input`
+    padding: 0.3em 1em;
+    ${(props) => !!props.groupRatio && css`flex: ${props.groupRatio}`};
+`;
+
+function Input(props) {
+    const {
+        onChange, onKeyPress, value, groupRatio
+    } = props;
+
+    return (
+        <StyledInput value={value}
+            groupRatio={groupRatio}
+            onChange={onChange}
+            onKeyPress={onKeyPress} />
+    );
+}
+
+Input.propTypes = {
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func,
+    onKeyPress: PropTypes.func,
+    groupRatio: PropTypes.number
+};
+
+Input.defaultProps = {
+    onChange: () => {
+    },
+    onKeyPress: () => {
+    },
+    groupRatio: 0
+};
+
+export default Input;
