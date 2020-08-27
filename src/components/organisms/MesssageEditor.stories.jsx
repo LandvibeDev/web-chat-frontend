0c82dcf4ccb0d356b3e967b8144c31d5a3ae8881
Seung Bin . Ko <kosb15@naver.com>
2020-08-27 16:59:13 +0900

    feat: 다양한 메시지 형식을 지원하기 윈한 MessageEditor 컴포넌트 추가 (#25)
    - 추후, 파일 메시지 같은 형식 이용시 에디터 항목 추가 예정

---
+++ src/components/organisms/MesssageEditor.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MessageEditor from './MessageEditor';
+
+export default {
+    title: 'components/organisms/MessageEditor',
+    component: MessageEditor
+};
+
+export const Standard = (args) => <MessageEditor {...args} />;
+Standard.args = {
+    send: () => {}
+};
