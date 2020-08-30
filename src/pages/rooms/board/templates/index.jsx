337fd8650bbd95a1070390010f75462144486bbd
Seung Bin . Ko <kosb15@naver.com>
2020-08-30 15:26:27 +0900

    refactor: RoomBoardTemplate 에서 Button 컴포넌트의 너비를 90% 변경 (#27)

--- src/pages/rooms/board/templates/index.jsx
+++ src/pages/rooms/board/templates/index.jsx
@@ -7,14 +7,16 @@ import { useHistory } from 'react-router-dom';
 const Container = styled.div`
   width: 100%;
   height: 100%;
+  text-align: center;
 `;
 
 function RoomBoardTemplate({ rooms }) {
     const history = useHistory();
+    const buttonStyle = { width: '90%', margin: '0 0.5rem' };
     return (
         <Container>
             <CardList rooms={rooms} />
-            <Button onClick={() => history.push('/rooms')} text="채팅방 생성" />
+            <Button onClick={() => history.push('/rooms')} text="채팅방 생성" styles={buttonStyle} />
         </Container>
     );
 }
