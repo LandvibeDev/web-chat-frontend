7fb67f78ee08f6370c2c295ad603a671d0e085f6
Seung Bin . Ko <kosb15@naver.com>
2020-08-30 15:16:38 +0900

    fix: 채팅방 생성 요청에서 누락된 body, 루트 경로 추가 (#28)

--- src/pages/rooms/maker/templates/index.jsx
+++ src/pages/rooms/maker/templates/index.jsx
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import 'css/RoomMake.css';
-import userIcon from "userIcon.PNG";
-import createRooms from "service/createRooms";
-import {useHistory} from 'react-router'
+import userIcon from 'userIcon.PNG';
+import createRooms from 'service/createRooms';
+import { useHistory } from 'react-router-dom';
 
 function RoomMakerTemplate() {
     const [title, setTitle] = useState('');
-    let history = useHistory();
+    const history = useHistory();
 
     function on(func) {
         return function (event) {
@@ -21,9 +21,8 @@ function RoomMakerTemplate() {
         if (title.length < 2 || title.length > 20) {
             alert('채팅방 생성이 실패하였습니다.');
         } else {
-            const isSuccesses = await createRooms();
+            const isSuccesses = await createRooms(title);
             if (isSuccesses) {
-                alert('채팅방이 생성되었습니다.');
                 history.goBack();
             } else {
                 alert('채팅방 생성이 실패하였습니다.');
@@ -35,23 +34,26 @@ function RoomMakerTemplate() {
 
     const onCreateClick = on(create);
 
-
     return (
         <div>
-            <div className='RoomMakeHeader'>
-                <label className='fontSetting'>서비스명</label>
-                <img className='userIcon' src={userIcon} alt=""/>
+            <div className="RoomMakeHeader">
+                <label className="fontSetting">서비스명</label>
+                <img className="userIcon" src={userIcon} alt="" />
             </div>
 
             <div>
-                <div className='enrollment'>
-                    <label className='radiusBox'>채팅방 개설</label>
-                    <div className='RoomMakeTitle'>
-                        <label className='fontSetting'>채팅방 명 : </label>
-                        <input className='RoomMakeInput' id="title" onChange={onChange} onKeyPress={onEnterPress}
-                               value={title} placeholder="입력해주세요."/>
+                <div className="enrollment">
+                    <label className="radiusBox">채팅방 개설</label>
+                    <div className="RoomMakeTitle">
+                        <label className="fontSetting">채팅방 명 : </label>
+                        <input className="RoomMakeInput"
+                            id="title"
+                            onChange={onChange}
+                            onKeyPress={onEnterPress}
+                            value={title}
+                            placeholder="입력해주세요." />
                     </div>
-                    <button className='RoomMakeBtn' onClick={onCreateClick}>생성</button>
+                    <button className="RoomMakeBtn" onClick={onCreateClick}>생성</button>
                 </div>
             </div>
         </div>
